5669bf3d79a6e7a43e3e04e60b89e1ba8e64c736
Aryav_Lahiri <aryav.lahiri@kcl.ac.uk>
2023-09-26 18:53:36 +0100

    Add a check for -ve numbers

--- primes.py
+++ primes.py
@@ -2,5 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes <= 0:
+        raise ValueError("Please only input Positive Numbers Only.")
+
+    list = [2]
+    
+    numbers = number_of_primes*5
+    
+    for number in range(3, numbers):
+        is_Prime = True 
+        for i in range(2,(number//2)+1):
+            if number % i == 0:
+                is_Prime = False
+
+        if is_Prime == True and len(list) < number_of_primes:
+            list.append(number)
+
+
+
     return list
